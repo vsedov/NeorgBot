24b831a74f00075ef454fffeefe3f79ff7304a1b
Binx-Codes <Devchou2008@gmail.com>
2021-07-30 21:49:17 +0400

    feat: add neorg spec commands

--- cogs/neorg_cmds.py
+++ cogs/neorg_cmds.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import discord
 
-page_types = {
+wiki_pages = {
 	'neorg': 'https://github.com/vhyrro/neorg/wiki',
 	'installation': 'https://github.com/vhyrro/neorg/wiki/Installation#installation',
 	'configuring-modules': 'https://github.com/vhyrro/neorg/wiki/Configuring-Modules',
@@ -25,6 +25,33 @@ page_types = {
 	'metamodules': 'https://github.com/vhyrro/neorg/wiki/Metamodules',
 }
 
+spec_pages = {
+	'spec': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md',
+	'theory': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#theory',
+	'design-decisions': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#design-decisions',
+	'parsing-order': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#parsing-order',
+	'attached-modifiers-and-their-functions': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#attached-modifiers-and-their-functions',
+	'detached-modifiers-and-their-functions': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#detached-modifiers-and-their-functions',
+	'trailing-modifiers': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#trailing-modifiers',
+	'escaping-special-characters': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#escaping-special-characters',
+	'defining-data': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#defining-data',
+	'tag-parameters': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#tag-parameters',
+	'carryover-tags': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#carryover-tags',
+	'quirks': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#quirks',
+	'single-line-paragraphs': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#single-line-paragraphs',
+	'intersecting-modifiers': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#intersecting-modifiers',
+	'lists': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#lists',
+	'nesting': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#nesting',
+	'todo-lists': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#todo-lists',
+	'links': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#links',
+	'the-first-segment': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#the-first-segment',
+	'the-second-segment': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#the-second-segment',
+	'markers': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#markers',
+	'drawers': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#drawers',
+	'data-tags': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#data-tags',
+	'unsupported-tags': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md#unsupported-tags',
+}
+
 class neorg_cmds(commands.Cog):
 
 	def __init__(self, bot):
@@ -33,7 +60,15 @@ class neorg_cmds(commands.Cog):
 	@commands.command()
 	async def wiki(self, ctx, *, query):
 		query = query.strip().lower().replace(' ', '-')
-		stuff = [page_types[k] for k in page_types.keys() if query in k]
+		stuff = [wiki_pages[k] for k in wiki_pages.keys() if query in k]
+		for i in stuff:
+			em = discord.Embed(description=i, colour=0x4878BE)
+			await ctx.send(embed=em)
+
+	@commands.command()
+	async def spec(self, ctx, *, query):
+		query = query.strip().lower().replace(' ', '-')
+		stuff = [spec_pages[k] for k in spec_pages.keys() if query in k]
 		for i in stuff:
 			em = discord.Embed(description=i, colour=0x4878BE)
 			await ctx.send(embed=em)
