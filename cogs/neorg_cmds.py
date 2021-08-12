58a2b65136a747f19f8b08fd22ffcc4583388613
tamton-aquib <aquibjavedt007@gmail.com>
2021-08-12 14:16:23 +0530

    dynamic wiki help

--- cogs/neorg_cmds.py
+++ cogs/neorg_cmds.py
@@ -1,29 +1,7 @@
 from discord.ext import commands
 import discord
-
-wiki_pages = {
-	'neorg': 'https://github.com/vhyrro/neorg/wiki',
-	'installation': 'https://github.com/vhyrro/neorg/wiki/Installation#installation',
-	'configuring-modules': 'https://github.com/vhyrro/neorg/wiki/Configuring-Modules',
-	'modules-concept': 'https://github.com/vhyrro/neorg/wiki/Installation#the-concept-of-modules',
-	'enabling-modules': 'https://github.com/vhyrro/neorg/wiki/Installation#enabling-our-own-modules',
-	'logger-config': 'https://github.com/vhyrro/neorg/wiki/Installation#configuring-the-logger',
-	'user-callbacks': 'https://github.com/vhyrro/neorg/wiki/User-Callbacks',
-	'user-keybinds': 'https://github.com/vhyrro/neorg/wiki/User-Keybinds',
-	'custom-highlights': 'https://github.com/vhyrro/neorg/wiki/Custom-Highlights',
-	'concealing': 'https://github.com/vhyrro/neorg/wiki/Concealing',
-	'workspace-management': 'https://github.com/vhyrro/neorg/wiki/Workspace-Management',
-	'creating-modules': 'https://github.com/vhyrro/neorg/wiki/Creating-Modules',
-	'autocommands': 'https://github.com/vhyrro/neorg/wiki/Autocommands',
-	'keybinds': 'https://github.com/vhyrro/neorg/wiki/Keybinds',
-	'neorg-command': 'https://github.com/vhyrro/neorg/wiki/Neorg-Command',
-	'concealing-api-functions': 'https://github.com/vhyrro/neorg/wiki/Concealing#api-functions',
-	'dirman': 'https://github.com/vhyrro/neorg/wiki/Dirman',
-	'api-calls-for-corehighlights': 'https://github.com/vhyrro/neorg/wiki/Custom-Highlights#api-calls-for-corehighlights',
-	'hotswapping-modules': 'https://github.com/vhyrro/neorg/wiki/Hotswapping-Modules',
-	'public-vs-public-config': 'https://github.com/vhyrro/neorg/wiki/Public-vs-Public-Config',
-	'metamodules': 'https://github.com/vhyrro/neorg/wiki/Metamodules',
-}
+import requests
+from bs4 import BeautifulSoup
 
 spec_pages = {
 	'spec': 'https://github.com/vhyrro/neorg/blob/main/docs/NFF-0.1-spec.md',
@@ -60,7 +38,22 @@ class neorg_cmds(commands.Cog):
 	@commands.command()
 	async def wiki(self, ctx, *, query):
 		query = query.strip().lower().replace(' ', '-')
-		wiki = [wiki_pages[k] for k in wiki_pages.keys() if query in k]
+		neorg_wiki = {}
+		wiki_url = "https://github.com/vhyrro/neorg/wiki"
+
+		stuff = BeautifulSoup(requests.get(wiki_url).text, 'lxml')
+		lis = stuff.find_all("div", {"class": "Box-body wiki-custom-sidebar markdown-body"})[0]
+
+		for li in lis.find_all('li'):
+			part = li.a['href']
+			neorg_wiki[part[37:].lower()] = part
+
+		wiki = [neorg_wiki[k] for k in neorg_wiki.keys() if query in k.lower()]
+
+		if len(wiki) == 0:
+			await ctx.send(embed=discord.Embed(description="No Results Found!", colour=0x4878BE))
+			return
+		
 		for i in wiki:
 			em = discord.Embed(description=i, colour=0x4878BE)
 			await ctx.send(embed=em)
