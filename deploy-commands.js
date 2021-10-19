f65c5dad52fb67006f27321efcb8a5805cf4c919
tamton-aquib <aquibjavedt007@gmail.com>
2021-10-19 17:56:08 +0530

    adding extra basic setup

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,18 +1,19 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+require("dotenv").config()
 
 const commands = [
-    new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-    new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-    new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
 ]
-    .map(command => command.toJSON());
+	.map(command => command.toJSON());
 
-const rest = new REST({ version: '9' }).setToken("token");
-const clientId = "client_id"
-const guildId = "guild_id"
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+const clientId = process.env.CLIENT_ID
+const guildId = process.env.GUILD_ID
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then(() => console.log('Successfully registered application commands.'))
-    .catch(console.error);
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
