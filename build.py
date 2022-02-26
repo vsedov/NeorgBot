3be2e70d1fdfcfd80ae52e558831c14195f252af
vsedov <viv.sedov@hotmail.com>
2022-02-26 12:40:39 +0000

    PyProject.toml

--- build.py
+++ build.py
@@ -1,12 +1,11 @@
 #   -*- coding: utf-8 -*-
-from pybuilder.core import use_plugin, init
+from pybuilder.core import init, use_plugin
 
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-
 
 name = "neorg-discord-bot"
 default_task = "publish"
