8a9075637bd44ef3332f82b9ba834908847ee236
vsedov <viv.sedov@hotmail.com>
2022-04-05 06:02:14 +0100

    Feat(script): new fuzzy search script for awesome neovim repo, to search plugins through that

--- src/main/python/hosting.py
+++ src/main/python/hosting.py
@@ -1,15 +1,36 @@
-from flask import Flask
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © Vivian Sedov
+#
+# File Name: hosting.py
 from threading import Thread
+
+from flask import Flask
 
 app = Flask('')
 
-@app.route('/')
-def main():
-    return "Your bot is ready"
+class FlaskThread:
 
-def run():
-    app.run(host="0.0.0.0", port=8000)
+    def __init__(self, host: str = "127.0.0.1", port: int = 8080):
+        self.host = host
+        self.port = port
 
-def keep_running():
-    server = Thread(target=run)
-    server.start()
+    @app.route('/')
+    def main():
+        return "Your bot is ready"
+
+    def run(self):
+        app.run(host=self.host, port=self.port, debug=True, use_reloader=False)
+
+    def start(self):
+        setup = Thread(target=self.run)
+        setup.start()
+        return setup
+
+    def __enter__(self):
+        return self
+
+    def stop(self):
+        app.stop()
