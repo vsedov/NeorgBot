345b5c33152caa6a34b034765479de659086cac1
vsedov <viv.sedov@hotmail.com>
2022-04-05 06:01:03 +0100

    chore(dependencies) update dependencies for pyhton env

--- build.py
+++ build.py
@@ -6,11 +6,17 @@ use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
+use_plugin("pypi:vulture")
+use_plugin("pypi:yapf")
+use_plugin("pypi:interrogate")
 
 name = "neorg-discord-bot"
 default_task = "publish"
 
-
 @init
 def set_properties(project):
-    pass
+    project.build_depends_on('coverage')
+    project.depends_on('flask')
+
+    project.set_property('coverage_break_build', False)
+    project.set_property('flake8_break_build', False)
