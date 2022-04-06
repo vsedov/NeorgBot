b850379325e1b210f02e63d92db6276dce59a449
vsedov <viv.sedov@hotmail.com>
2022-04-06 10:27:44 +0100

    refactor(neorg): refactor neorg into class, allows modularity

--- neorg/neorg.py
+++ neorg/neorg.py
@@ -5,18 +5,16 @@
 # File Name: neorg.py
 import asyncio
 import logging
-import os
+import warnings
 
+import constants
 import discord
-import pyinspect as pi
 from discord.ext import commands
 from icecream import ic
-
-from .log import get_logger
+from log import get_logger
 
 log = get_logger("norg")
 LOCALHOST = "127.0.0.1"
-
 
 class StartupError(Exception):
     """Exception class for startup errors."""
@@ -25,71 +23,72 @@ class StartupError(Exception):
         super().__init__()
         self.exception = base
 
-class Norg(commands.Bot):
-    """A subclass of discord.ext.command.Bots where aiohttp session and api client will be called."""
+class Neorg(commands.Bot):
 
     def __init__(self, *args, **kwargs):
+        if "connector" in kwargs:
+            warnings.warn(
+                "If login() is called (or the bot is started), the connector will be overwritten "
+                "with an internal one")
+
         super().__init__(*args, **kwargs)
-        #  TODO(vsedov) (05:11:05 - 06/04/22): use aiohttp for client for http addr
 
     @classmethod
     def create(cls) -> "Neorg":
-        "Create and return a instance of Neorg bot"
+        """Create and return an instance of a Bot."""
         loop = asyncio.get_event_loop()
+        intents = discord.Intents.all()
+        intents.dm_typing = False
+        intents.dm_reactions = False
+        intents.invites = False
+        intents.webhooks = False
+        intents.integrations = False
+
         return cls(
-            #  TODO(vsedov) (05:13:45 - 06/04/22): Call from global file called constants
-            command_prefix=commands.when_mentioned_or("n!"),
-            description="Neorg",
             loop=loop,
-            intents=discord.Intents.all())
+            command_prefix=commands.when_mentioned_or(constants.Neorg.prefex),
+            activity=discord.Game(name=f"Commands: {constants.Neorg.prefex}help"),
+            case_insensitive=True,
+            max_messages=10_000,
+            allowed_mentions=discord.AllowedMentions(
+                everyone=False, roles=constants.MODERATION_ROLES),
+            intents=intents,
+        )
 
+    def load_cogs(self) -> None:
+        """Load all cogs."""
+        from utils.extensions import EXTENSIONS
+        print(EXTENSIONS)
+        for extension in EXTENSIONS:
+            try:
+                self.load_extension(extension)
+            except Exception as e:
+                log.error(f"Failed to load extension {extension}", exc_info=e)
 
-client = commands.Bot(command_prefix=['n.', 'N.'])
+    def add_cog(self, cog: commands.Cog) -> None:
+        super().add_cog(cog)
+        log.info(f"Cog loaded: {cog.qualified_name}")
 
-extensions = ['cogs.help', 'cogs.neorg_cmds', 'cogs.general']
-def client_load_extensions():
-    for e in extensions:
-        logging.info(ic.format("loading extension: {}", e))
-        client.load_extension(e)
+    async def wait_till_ready(self) -> None:
+        """Wait until the bot is ready."""
+        await super().wait_till_ready()
+        log.info("Bot is ready")
+        await self.change_presence(
+            status=discord.Status.online, activity=discord.Game('the prefix n. | n.help'))
+        log.info("Bot is ready")
 
-@client.event
-async def on_ready():
-    await client.change_presence(
-        status=discord.Status.online, activity=discord.Game('the prefix n. | n.help'))
-    logging.info(ic.format("logged in as: {}", client.user))
-
-@client.command(aliases=['r'])
-async def reload(ctx, cog):
-    """ reload cogs
-
-    Parameters
-    ----------
-    ctx : ctx : discord.ext.commands.Context
-        The context of the command
-    cog : Cog : discord.ext.commands.Cog
-        external modules to reload
-
-    Returns
-    -------
-    None
-    """
-    if not cog:
-        await ctx.send('Specify the cog to reload!')
-        return
-    try:
-        client.unload_extension(f'cogs.{cog}')
-        client.load_extension(f'cogs.{cog}')
-        await ctx.send(
-            embed=discord.Embed(
-                description=f"Cog **{cog}** reloaded", colour=discord.Color.red()))
-        # this is a hack to reload the cog without restarting the bot
-    except Exception as ae:
-        await ctx.send(ae)
-        logging.warning(ic.format(f"{cog} could not be loaded"))
-
-if __name__ == "__main__":
-    pi.install_traceback(enable_prompt=True)
-    hosting.keep_running()
-    __import__("dotenv").load_dotenv()
-    client_load_extensions()
-    client.run(os.environ['TOKEN'])
+    @commands.command(name="reload")
+    async def reload(self, ctx, cog):
+        if not cog:
+            await ctx.send('Specify the cog to reload!')
+            return
+        try:
+            self.unload_extension(f'cogs.{cog}')
+            self.load_extension(f'cogs.{cog}')
+            await ctx.send(
+                embed=discord.Embed(
+                    description=f"Cog **{cog}** reloaded", colour=discord.Color.red()))
+            # this is a hack to reload the cog without restarting the bot
+        except Exception as ae:
+            await ctx.send(ae)
+            logging.warning(ic.format(f"{cog} could not be loaded"))
