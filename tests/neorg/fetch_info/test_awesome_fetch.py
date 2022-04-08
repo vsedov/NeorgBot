390a28b88aaaac50cb8c17740399112227e1bfc2
vsedov <viv.sedov@hotmail.com>
2022-04-08 15:49:40 +0100

    test(initial): setup modules for fun and neorg_wiki

--- tests/neorg/fetch_info/test_awesome_fetch.py
+++ tests/neorg/fetch_info/test_awesome_fetch.py
@@ -4,8 +4,10 @@ from neorg.fetch_info.fetch_from_awesome import ReadAwesome, weak_lru
 
 
 class TestMessages(unittest.TestCase):
+    """ Test the awesome neovim fuzzy search. """
 
     def test_fetch_info_lsp(self):
+        """ Search for lsp, should return a bunch of lsp based plugins. """
         message_lsp = ReadAwesome().fuzzy('lsp')
         # format message first
         for name, desc in message_lsp.items():
@@ -15,10 +17,12 @@ class TestMessages(unittest.TestCase):
         self.assertGreater(len(message_lsp), 2)
 
     def test_null_case(self):
+        """ Test Null case if nothign is found. """
         message_null = ReadAwesome().fuzzy('')
         self.assertEqual(message_null, {})
 
     def test_weak_lur(self):
+        """LRU test, cache test."""
 
         @weak_lru(maxsize=128, typed=False)
         def func(self, *args, **kwargs):
@@ -36,10 +40,12 @@ class TestMessages(unittest.TestCase):
         self.assertEqual(func(A()), 'A')
 
     def test_get_header(self):
+        """ return valid headers i.e dictionary of information from the site. """
         diction = ReadAwesome().get_from_header()
         self.assertGreater(len(diction), 20)
 
     def test_contains_atleast_one_link(self):
+        """ Test if the returned values from header contains a link, that we can refer to. """
         diction = ReadAwesome().get_from_header()
         for x in diction.values():
             self.assertIsInstance(x, dict)
