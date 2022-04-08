8225ea0898ccbf8673031fabeef0ad3c50f1f934
vsedov <viv.sedov@hotmail.com>
2022-04-08 15:33:05 +0100

    refactor(general cogs): Change package name to Fun as its not related to general content

---
+++ neorg/cogs/fun/fun.py
@@ -0,0 +1,39 @@
+# flake8: noqa
+import discord
+from discord.ext.commands import Cog
+
+
+class FunListen(Cog):
+    """General Commansd and event inspection"""
+
+    def __init__(self, bot:discord.ext.commands.Bot):
+        self.bot = bot
+
+    @Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        """on message listens to events and messages on server and puts sus as a reaction"""
+        if 'sus' in message.content.lower():
+            await message.add_reaction("<:sus:867395030988881921>")
+
+    @Cog.listener()
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent) -> None:
+        """on raw reaction add listens to events and checks payload. it checks if a message has a reaction.
+
+        Parameters
+        ----------
+        payload : discord.RawReactionActionEvent
+            The payload of the event.
+        """
+        if payload.emoji.name in '📑🔖':
+            msg = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+            author = msg.author
+
+            if msg.content != "":
+                bookmark = discord.Embed(description=msg.content, colour=0x4878BE)
+                bookmark.set_author(name=author.name, icon_url=author.avatar_url)
+                user = await self.bot.fetch_user(payload.user_id)
+                await user.send(embed=bookmark)
+
+
+def setup(bot: discord.ext.commands.Bot) -> None:
+    bot.add_cog(FunListen(bot))
